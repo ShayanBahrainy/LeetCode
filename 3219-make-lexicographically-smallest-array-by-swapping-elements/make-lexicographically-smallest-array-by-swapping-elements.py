1745fd8a5b4220aa164dfc0bd94e5c997724f378
Shayan B. <64701891+ShayanBahrainy@users.noreply.github.com>
2025-01-25 15:56:46 -0800

    Time: 2033 ms (5.10%) | Memory: 51.8 MB (40.77%) - LeetSync

--- 3219-make-lexicographically-smallest-array-by-swapping-elements/make-lexicographically-smallest-array-by-swapping-elements.py
+++ 3219-make-lexicographically-smallest-array-by-swapping-elements/make-lexicographically-smallest-array-by-swapping-elements.py
@@ -2,7 +2,7 @@ class Solution:
     def lexicographicallySmallestArray(self, nums: List[int], limit: int) -> List[int]:
         sort_nums = sorted(nums)
         num_to_group = {}
-        group_to_members = [[] for _ in range(len(nums))]
+        group_to_members = {0: []}
         group_index = 0
         group_to_members[0].append(sort_nums[0])
         num_to_group[sort_nums[0]] = 0
@@ -10,6 +10,8 @@ class Solution:
             num = sort_nums[i]
             if abs(sort_nums[i - 1] - num) > limit:
                 group_index += 1
+            if group_index not in group_to_members:
+                group_to_members[group_index] = []
             group_to_members[group_index].append(num)
             num_to_group[num] = group_index
         for i in range(len(nums)):
