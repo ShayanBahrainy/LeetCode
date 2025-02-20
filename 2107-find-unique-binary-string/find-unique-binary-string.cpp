d514a74ba60c16e3ebb9d2582d56c80e8ba9c41a
Shayan B. <64701891+ShayanBahrainy@users.noreply.github.com>
2025-02-19 18:13:30 -0800

    Time: 0 ms (100.00%) | Memory: 12.6 MB (84.00%) - LeetSync

--- 2107-find-unique-binary-string/find-unique-binary-string.cpp
+++ 2107-find-unique-binary-string/find-unique-binary-string.cpp
@@ -4,7 +4,7 @@ public:
     string findDifferentBinaryString(vector<string>& nums) {
         ostringstream different;
         for (int i = 0; i < nums.size(); ++i) {
-            different << (nums[i][i] == '1' ?  "0": "1");
+            different << (nums[i][i] == '1' ?  '0': '1');
         }
         return different.str();
     }
