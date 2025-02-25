bbc9411340eaa34a723384f42f86bbfcb8b20a6c
Shayan B. <64701891+ShayanBahrainy@users.noreply.github.com>
2025-02-25 11:15:56 -0800

    Time: 2035 ms (5.23%) | Memory: 112.2 MB (32.08%) - LeetSync

---
+++ 1631-number-of-sub-arrays-with-odd-sum/number-of-sub-arrays-with-odd-sum.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int numOfSubarrays(vector<int>& arr) {
+        int possible = 0;
+        const int MOD = 1e9 + 7;
+        int prefixSum = 0;
+        int evens = 1;
+        int odds = 0;
+
+        for (int i = 0; i < arr.size(); ++i) {
+            prefixSum += arr[i];
+            if (prefixSum % 2 == 0) {
+                possible += odds;
+                evens++;
+            }
+            else {
+                possible += evens;
+                odds++;
+            }
+            possible %= MOD;
+            cout << possible << endl;
+        }
+        return possible;
+    }
+};
