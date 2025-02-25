e7ed52829c564071b272b6709fa24fb6b7071c1e
Shayan B. <64701891+ShayanBahrainy@users.noreply.github.com>
2025-02-25 10:36:23 -0800

    Time: 0 ms (100.00%) | Memory: 8 MB (18.20%) - LeetSync

---
+++ 2556-convert-the-temperature/convert-the-temperature.c
@@ -0,0 +1,12 @@
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+double* convertTemperature(double celsius, int* returnSize) {
+    *returnSize = 2;
+    double* Johnny = malloc(sizeof(double) * (*returnSize));
+
+    Johnny[0] = celsius + 273.15;
+    Johnny[1] = (celsius * 1.80) + 32.0;
+
+    return Johnny;
+}
