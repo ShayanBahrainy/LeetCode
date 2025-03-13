e079df78dc25a94d84bdf0ef029257365c12a047
Shayan B. <64701891+ShayanBahrainy@users.noreply.github.com>
2025-03-12 21:03:03 -0700

    Time: 104 ms (27.44%) | Memory: 56.1 MB (15.90%) - LeetSync

--- 3639-zero-array-transformation-i/zero-array-transformation-i.py
+++ 3639-zero-array-transformation-i/zero-array-transformation-i.py
@@ -6,7 +6,6 @@ class Solution:
             difference_array[query[1] + 1] -= 1
         for i in range(1, len(difference_array)):
             difference_array[i] += difference_array[i - 1]
-        print(difference_array)
         for i in range(len(nums)):
             if difference_array[i] < nums[i]:
                 return False
