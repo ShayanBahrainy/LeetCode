0d339e7c42bcaf00dc4a064c9f3541caa1d5d0d7
Shayan B. <64701891+ShayanBahrainy@users.noreply.github.com>
2025-03-12 21:02:51 -0700

    Time: 127 ms (18.81%) | Memory: 55.3 MB (38.88%) - LeetSync

---
+++ 3639-zero-array-transformation-i/zero-array-transformation-i.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isZeroArray(self, nums: List[int], queries: List[List[int]]) -> bool:
+        difference_array = [0 for _ in range(len(nums) + 1)]
+        for query in queries:
+            difference_array[query[0]] += 1
+            difference_array[query[1] + 1] -= 1
+        for i in range(1, len(difference_array)):
+            difference_array[i] += difference_array[i - 1]
+        print(difference_array)
+        for i in range(len(nums)):
+            if difference_array[i] < nums[i]:
+                return False
+        return True
